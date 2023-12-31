9ed9abfbb6bd2c2af584b51b722d6b8ca6f6c123
nuuuri <sbfl125@gmail.com>
2023-12-31 23:58:37 +0900

    [feat] Redis-YouTube API 연동

--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -1,23 +1,67 @@
 import axios from 'axios';
 
-export async function GET() {
-  try {
-    const res = await axios.get(
-      'https://www.googleapis.com/youtube/v3/search',
-      {
-        params: {
-          key: process.env.YOUTUBE_API_KEY,
-          q: '티원',
-          part: 'snippet',
-          type: 'video',
-          maxResults: 25,
-          fields: 'items(id, snippet(title))',
-          videoEmbeddable: true,
-        },
-      },
-    );
+import { redisConnect } from 'lib/redisConnect';
 
-    const data = res.data;
+export async function GET() {
+  const len = await redisConnect.llen('youtube');
+
+  const data = { items: [] as any[] };
+
+  try {
+    if (len) {
+      (await redisConnect.lrange('youtube', 0, -1)).forEach((item) => {
+        data.items.push(JSON.parse(item));
+      });
+    } else {
+      const res1 = await axios.get(
+        'https://www.googleapis.com/youtube/v3/search',
+        {
+          params: {
+            key: process.env.YOUTUBE_API_KEY,
+            q: 'T1',
+            part: 'snippet',
+            type: 'video',
+            maxResults: 25,
+            fields: 'items(id, snippet(title))',
+            videoEmbeddable: true,
+          },
+        },
+      );
+      const res2 = await axios.get(
+        'https://www.googleapis.com/youtube/v3/search',
+        {
+          params: {
+            key: process.env.YOUTUBE_API_KEY,
+            q: '티원',
+            part: 'snippet',
+            type: 'video',
+            maxResults: 25,
+            fields: 'items(id, snippet(title))',
+            videoEmbeddable: true,
+          },
+        },
+      );
+      const res3 = await axios.get(
+        'https://www.googleapis.com/youtube/v3/search',
+        {
+          params: {
+            key: process.env.YOUTUBE_API_KEY,
+            q: '페이커',
+            part: 'snippet',
+            type: 'video',
+            maxResults: 25,
+            fields: 'items(id, snippet(title))',
+            videoEmbeddable: true,
+          },
+        },
+      );
+
+      data.items = [...res1.data.items, ...res2.data.items, ...res3.data.items];
+
+      data.items.forEach((item) => {
+        redisConnect.lpush('youtube', JSON.stringify(item));
+      });
+    }
 
     return new Response(JSON.stringify({ videos: data.items }), {
       status: 200,
